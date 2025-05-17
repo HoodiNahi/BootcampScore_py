c5379c160d6e9cc2669aee6ae9711de54ad4589e
HoodiNahi <jbhatt1212@gmail.com>
2025-05-17 15:42:13 +0100

    change fetchDatesForPilot to fetchWeaponsForPilot

--- main.py
+++ main.py
@@ -36,19 +36,19 @@ def get_pilots(db: Session = Depends(get_db)):
     pilots = db.query(models.Pass.name).distinct().all()
     return [p[0] for p in pilots]
 
-@app.get("/dates/", response_model=List[str])
+@app.get("/weapons/", response_model=List[str])
 def get_dates(pilots: List[str] = Query(...), db: Session = Depends(get_db)):
-    dates = (
-        db.query(models.Pass.date)
+    weapons = (
+        db.query(models.Pass.weapon)
         .filter(models.Pass.name.in_(pilots))
         .distinct()
         .all()
     )
-    return [d[0] for d in dates]
+    return [w[0] for w in weapons]
 
-@app.get("/passes/{pilot}/{date}", response_model=List[schemas.PassOut])
-def get_passes(pilot: str, date: str, db: Session = Depends(get_db)):
-    return db.query(models.Pass).filter_by(name=pilot, date=date).all()
+@app.get("/passes/{pilot}/{weapon}", response_model=List[schemas.PassOut])
+def get_passes(pilot: str, weapon: str, db: Session = Depends(get_db)):
+    return db.query(models.Pass).filter_by(name=pilot, weapon=weapon).all()
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
